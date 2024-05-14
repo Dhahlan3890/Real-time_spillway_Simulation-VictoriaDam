635ca610619696bf3bd3afd890b77aca3f85b739
Dhahlan3890 <dhahlan2000@gmail.com>
2024-05-14 20:51:27 +0530

    commented unnecessary codes

--- Realtime_victoria_dam_Simulaion.py
+++ Realtime_victoria_dam_Simulaion.py
@@ -148,42 +148,7 @@ class Droplet(pygame.sprite.Sprite):
                               random.uniform(water_fall_start8/1000*WIDTH, (water_fall_start8+box1_width)/1000*WIDTH) ]
             self.rect.x = random.choice(x_values)
 
-"""
 
-class WaterParticle(pygame.sprite.Sprite):
-    def __init__(self, x, y):
-        super().__init__()
-        self.image = pygame.Surface((5, 5))
-        self.image.fill(BLUE)
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
-
-    def update(self):
-        # Simulate water flow by updating particle position
-        self.rect.y += 1
-        if self.rect.y > HEIGHT:
-            self.rect.y = 0
-
-
-
-class watermove(pygame.sprite.Sprite):
-    def __init__(self, x, y):
-        super().__init__()
-        self.image = pygame.Surface((1.5, 5))
-        self.image.fill(BLUE)
-        self.rect = self.image.get_rect()
-        self.rect.center = (x, y)
-        self.height = random.uniform(0,1)
-
-    def update(self):
-        self.rect.height += self.height
-        self.rect.x = random.uniform(0,100)
-        if self.rect.y > 1000:
-            self.rect.y = box1_y + 95
-            self.rect.x = random.uniform(0,100)
-
-"""
 
 # Create sprite groups
 all_sprites = pygame.sprite.Group()
@@ -192,15 +157,6 @@ all_sprites = pygame.sprite.Group()
 for _ in range(20000):
     droplet = Droplet(random.randint(0, 0), random.randint(100, HEIGHT))
     all_sprites.add(droplet)
-"""
-for i in range(10000):
-    particle = WaterParticle(i * 8, 0)
-    all_sprites.add(particle)
-
-for i in range(10000):
-    particle = watermove(i * 8, 0)
-    all_sprites.add(particle)
-"""
 
 ##############################################################
 
@@ -248,109 +204,7 @@ while True:
     
 
 
-    """"
-
-    L1,L2,L3,L4,L5,L6,L7,L8,L9,L10,L11,L12,L13,L14,L15,L16,L17,L18,L19,L20,L21,L22,L23,L24,L25,L26,L27,L28,L29,L30,L31,L32 = [RED]*32
-
-    if box_height > 438.65:
-        L1, L3,L2,L5, L4,L7, L6, L9, L8, L11, L10, L13, L12, L15, L14, L17, L16, L19, L18, L21, L20, L23, L22, L25, L24, L27, L26, L29, L28, L31, L30, L32 = [GREEN] * 32
-        #print("gate L32 opened")
-    elif box_height > 438.61:
-        L1, L3,L2,L5, L4,L7, L6, L9, L8, L11, L10, L13, L12, L15, L14, L17, L16, L19, L18, L21, L20, L23, L22, L25, L24, L27, L26, L29, L28, L31, L30 = [GREEN] * 31
-        #print("gate L30 opened")
-    elif box_height > 438.60:
-        L1, L3,L2,L5, L4,L7, L6, L9, L8, L11, L10, L13, L12, L15, L14, L17, L16, L19, L18, L21, L20, L23, L22, L25, L24, L27, L26, L29, L28, L31 = [GREEN] * 30
-        #print("gate L31 opened")
-    elif box_height > 438.57:
-        L1, L3,L2,L5, L4,L7, L6, L9, L8, L11, L10, L13, L12, L15, L14, L17, L16, L19, L18, L21, L20, L23, L22, L25, L24, L27, L26, L29, L28 = [GREEN] * 29
-        #print("gate L28 opened")
-    elif box_height > 438.56:
-        L1, L3,L2,L5, L4,L7, L6, L9, L8, L11, L10, L13, L12, L15, L14, L17, L16, L19, L18, L21, L20, L23, L22, L25, L24, L27, L26, L29 = [GREEN] * 28
-        #print("gate L29 opened")
-    elif box_height > 438.53:
-        L1, L3,L2,L5, L4,L7, L6, L9, L8, L11, L10, L13, L12, L15, L14, L17, L16, L19, L18, L21, L20, L23, L22, L25, L24, L27, L26 = [GREEN] * 27
-        #print("gate L26 opened")
-    elif box_height > 438.52:
-        L1, L3,L2,L5, L4,L7, L6, L9, L8, L11, L10, L13, L12, L15, L14, L17, L16, L19, L18, L21, L20, L23, L22, L25, L24, L27 = [GREEN] * 26
-        #print("gate L27 opened")
-    elif box_height > 438.49:
-        L1, L3,L2,L5, L4,L7, L6, L9, L8, L11, L10, L13, L12, L15, L14, L17, L16, L19, L18, L21, L20, L23, L22, L25, L24 = [GREEN] * 25
-        #print("gate L24 opened")
-    elif box_height > 438.48:
-        L1, L3,L2,L5, L4,L7, L6, L9, L8, L11, L10, L13, L12, L15, L14, L17, L16, L19, L18, L21, L20, L23, L22, L25 = [GREEN] * 24
-        #print("gate L25 opened")
-    elif box_height > 438.45:
-        L1, L3,L2,L5, L4,L7, L6, L9, L8, L11, L10, L13, L12, L15, L14, L17, L16, L19, L18, L21, L20, L23, L22 = [GREEN] * 23
-        #print("gate L22 opened")
-    elif box_height > 438.44:
-        L1, L3,L2,L5, L4,L7, L6, L9, L8, L11, L10, L13, L12, L15, L14, L17, L16, L19, L18, L21, L20, L23 = [GREEN] * 22
-        #print("gate L23 opened")
-    elif box_height > 438.41:
-        L1, L3,L2,L5, L4,L7, L6, L9, L8, L11, L10, L13, L12, L15, L14, L17, L16, L19, L18, L21, L20 = [GREEN] * 21
-        #print("gate L20 opened")
-    elif box_height > 438.40:
-        L1, L3,L2,L5, L4,L7, L6, L9, L8, L11, L10, L13, L12, L15, L14, L17, L16, L19, L18, L21 = [GREEN] * 20
-        #print("gate L21 opened")
-    elif box_height > 438.37:
-        L1, L3,L2,L5, L4,L7, L6, L9, L8, L11, L10, L13, L12, L15, L14, L17, L16, L19, L18 = [GREEN] * 19
-        #print("gate L18 opened")
-    elif box_height > 438.36:
-        L1, L3,L2,L5, L4,L7, L6, L9, L8, L11, L10, L13, L12, L15, L14, L17, L16, L19 = [GREEN] * 18
-        #print("gate L19 opened")
-    elif box_height > 438.33:
-        L1, L3,L2,L5, L4,L7, L6, L9, L8, L11, L10, L13, L12, L15, L14, L17, L16 = [GREEN] * 17
-        #print("gate L16 opened")
-    elif box_height > 438.32:
-        L1, L3,L2,L5, L4,L7, L6, L9, L8, L11, L10, L13, L12, L15, L14, L17 = [GREEN] * 16
-        #print("gate L17 opened")
-    elif box_height > 438.29:
-        L1, L3,L2,L5, L4,L7, L6, L9, L8, L11, L10, L13, L12, L15, L14 = [GREEN] * 15
-        #print("gate L14 opened")
-    elif box_height > 438.28:
-        L1, L3,L2,L5, L4,L7, L6, L9, L8, L11, L10, L13, L12, L15 = [GREEN] * 14
-        #print("gate L15 opened")
-    elif box_height > 438.25:
-        L1, L3,L2,L5, L4,L7, L6, L9, L8, L11, L10, L13, L12 = [GREEN] * 13
-        #print("gate L12 opened")
-    elif box_height > 438.24:
-        L1, L3,L2,L5, L4,L7, L6, L9, L8, L11, L10, L13 = [GREEN] * 12
-        #print("gate L13 opened")
-    elif box_height > 438.21:
-        L1, L3,L2,L5, L4,L7, L6, L9, L8, L11, L10 = [GREEN] * 11
-        #print("gate L10 opened")
-    elif box_height > 438.20:
-        L1, L3,L2,L5, L4,L7, L6, L9, L8, L11 = [GREEN] * 10
-        #print("gate L11 opened")
-    elif box_height > 438.17:
-        L1, L3,L2,L5, L4,L7, L6, L9, L8 = [GREEN] * 9
-        #print("gate L8 opened")
-    elif box_height > 438.16:
-        L1, L3,L2,L5, L4,L7, L6, L9 = [GREEN] * 8
-        #print("gate L9 opened")
-    elif box_height > 438.13:
-        L1, L3,L2,L5, L4,L7, L6 = [GREEN] * 7
-        #print("gate L6 opened")
-    elif box_height > 438.12:
-        L1, L3,L2,L5, L4,L7 = [GREEN] * 6
-        #print("gate L7 opened")
-    elif box_height > 438.09:
-        L1, L3,L2,L5, L4 = [GREEN] * 5
-        #print("gate L4 opened")
-    elif box_height > 438.08:
-        L1, L3,L2,L5 = [GREEN] * 4
-        #print("gate L5 opened")
-    elif box_height > 438.05:
-        L1, L3,L2 = [GREEN] * 3
-        #print("gate L2 opened")
-    elif box_height > 438.04:
-        L1, L3 = [GREEN] * 2
-        #print("gate L3 opened")
-    elif box_height > 438:
-        L1 = GREEN
-        #print("gate L1 opened")
-
-
-    """
+   
             
         
         
@@ -759,79 +613,11 @@ while True:
         screen.blit(height_text1, (globals()[f"box{i+1}_x"]/1000*WIDTH, (globals()[f"box{i+1}_y"]-20)/500*HEIGHT))
         pygame.draw.rect(screen, BLUE, (globals()[f"box{i+1}_x"]/1000*WIDTH, globals()[f"box{i+1}_y"]/500*HEIGHT + (95-globals()[f"box{i+1}_level"])/500*HEIGHT, globals()[f"box{i+1}_width"]/1000*WIDTH, globals()[f"box{i+1}_level"]/500*HEIGHT))
     # Draw the blue box
-    """
     
-    pygame.draw.rect(screen, box1_color, (box1_x, box1_y, box1_width, box1_height))
-    height_text1 = font1.render(f"{box1_level/10:.1f}", True, (0, 0, 0))
-    screen.blit(height_text1, (box1_x, box1_y-20))
-    pygame.draw.rect(screen, BLUE, (box1_x, box1_y + 95-box1_level, box1_width, box1_level))
-    pygame.draw.rect(screen, box2_color, (box2_x, box2_y, box2_width, box2_height))
-    height_text2 = font1.render(f"{box2_level/10:.1f}", True, (0, 0, 0))
-    screen.blit(height_text2, (box2_x, box2_y-20))
-    pygame.draw.rect(screen, BLUE, (box2_x, box1_y + 95-box2_level, box1_width, box2_level))
-    pygame.draw.rect(screen, box3_color, (box3_x, box3_y, box3_width, box3_height))
-    height_text3 = font1.render(f"{box3_level/10:.1f}", True, (0, 0, 0))
-    screen.blit(height_text3, (box3_x, box3_y-20))
-    pygame.draw.rect(screen, BLUE, (box3_x, box1_y + 95-box3_level, box1_width, box3_level))
-    pygame.draw.rect(screen, box4_color, (box4_x, box4_y, box4_width, box4_height))
-    height_text4 = font1.render(f"{box4_level/10:.1f}", True, (0, 0, 0))
-    screen.blit(height_text4, (box4_x, box4_y-20))
-    pygame.draw.rect(screen, BLUE, (box4_x, box1_y + 95-box4_level, box1_width, box4_level))
-    pygame.draw.rect(screen, box4_color, (box5_x, box5_y, box5_width, box5_height))
-    height_text5 = font1.render(f"{box5_level/10:.1f}", True, (0, 0, 0))
-    screen.blit(height_text5, (box5_x, box5_y-20))
-    pygame.draw.rect(screen, BLUE, (box5_x, box1_y + 95-box5_level, box1_width, box5_level))
-    pygame.draw.rect(screen, box4_color, (box6_x, box6_y, box6_width, box6_height))
-    height_text6 = font1.render(f"{box6_level/10:.1f}", True, (0, 0, 0))
-    screen.blit(height_text6, (box6_x, box6_y-20))
-    pygame.draw.rect(screen, BLUE, (box6_x, box1_y + 95-box6_level, box1_width, box6_level))
-    pygame.draw.rect(screen, box4_color, (box7_x, box7_y, box7_width, box7_height))
-    height_text7 = font1.render(f"{box7_level/10:.1f}", True, (0, 0, 0))
-    screen.blit(height_text7, (box7_x, box7_y-20))
-    pygame.draw.rect(screen, BLUE, (box7_x, box1_y + 95-box7_level, box1_width, box7_level))
-    pygame.draw.rect(screen, box4_color, (box8_x, box8_y, box8_width, box8_height))
-    height_text8 = font1.render(f"{box8_level/10:.1f}", True, (0, 0, 0))
-    screen.blit(height_text8, (box8_x, box8_y-20))
-    pygame.draw.rect(screen, BLUE, (box8_x, box1_y + 95-box8_level, box1_width, box8_level))
-
-    """
     for i in range(0,32):
         pygame.draw.rect(screen, globals()[f"L{i+1}"], ((750+50*(i%5))/1000*WIDTH, (50+50*(i//5))/500*HEIGHT, ls_width, ls_height))
 
-    """
-    pygame.draw.rect(screen, L1, (750/1000*WIDTH, 50, ls_width, ls_height))
-    pygame.draw.rect(screen, L2, ((750+50*1)/1000*WIDTH, 50, ls_width, ls_height))
-    pygame.draw.rect(screen, L3, (750+50*2, 50, ls_width, ls_height))
-    pygame.draw.rect(screen, L4, (750+50*3, 50, ls_width, ls_height))
-    pygame.draw.rect(screen, L5, (750+50*4, 50, ls_width, ls_height))
-    pygame.draw.rect(screen, L6, (750, 100, ls_width, ls_height))
-    pygame.draw.rect(screen, L7, (750+50, 100, ls_width, ls_height))
-    pygame.draw.rect(screen, L8, (750+50*2, 100, ls_width, ls_height))
-    pygame.draw.rect(screen, L9, (750+50*3, 100, ls_width, ls_height))
-    pygame.draw.rect(screen, L10, (750+50*4, 100, ls_width, ls_height))
-    pygame.draw.rect(screen, L11, (750, 150, ls_width, ls_height))
-    pygame.draw.rect(screen, L12, (750+50*1, 150, ls_width, ls_height))
-    pygame.draw.rect(screen, L13, (750+50*2, 150, ls_width, ls_height))
-    pygame.draw.rect(screen, L14, (750+50*3, 150, ls_width, ls_height))
-    pygame.draw.rect(screen, L15, (750+50*4, 150, ls_width, ls_height))
-    pygame.draw.rect(screen, L16, (750, 200, ls_width, ls_height))
-    pygame.draw.rect(screen, L17, (750+50*1, 200, ls_width, ls_height))
-    pygame.draw.rect(screen, L18, (750+50*2, 200, ls_width, ls_height))
-    pygame.draw.rect(screen, L19, (750+50*3, 200, ls_width, ls_height))
-    pygame.draw.rect(screen, L20, (750+50*4, 200, ls_width, ls_height))
-    pygame.draw.rect(screen, L21, (750, 250, ls_width, ls_height))
-    pygame.draw.rect(screen, L22, (750+50*1, 250, ls_width, ls_height))
-    pygame.draw.rect(screen, L23, (750+50*2, 250, ls_width, ls_height))
-    pygame.draw.rect(screen, L24, (750+50*3, 250, ls_width, ls_height))
-    pygame.draw.rect(screen, L25, (750+50*4, 250, ls_width, ls_height))
-    pygame.draw.rect(screen, L26, (750, 300, ls_width, ls_height))
-    pygame.draw.rect(screen, L27, (750+50*1, 300, ls_width, ls_height))
-    pygame.draw.rect(screen, L28, (750+50*2, 300, ls_width, ls_height))
-    pygame.draw.rect(screen, L29, (750+50*3, 300, ls_width, ls_height))
-    pygame.draw.rect(screen, L30, (750+50*4, 300, ls_width, ls_height))
-    pygame.draw.rect(screen, L31, (750, 350, ls_width, ls_height))
-    pygame.draw.rect(screen, L32, (750+50, 350, ls_width, ls_height))
-    """
+    
     for j in range(8):
         box_level = globals()[f"box{j+1}_level"]
         if box_level > 9.35 * 10:
@@ -872,12 +658,7 @@ while True:
             height_text1 = font2.render(f"Vx{i+1}", True, GS[j][i])
             screen.blit(height_text1, ((320 + 50*j)/1000*WIDTH, (80 - 12*i)/500*HEIGHT))
 
-    """
-    for i in range(WIDTH//2):
-        vibration = random.uniform(0,10)
-        pygame.draw.rect(screen, (0,0,255,200), (200+i, 200, 5/1000*WIDTH, water_height-vibration))
-        pygame.draw.rect(screen, (0,0,255,200), (water_x, water_y, water_width, water_height))
-    """
+  
     
 
     for i in range(0,32):
@@ -889,73 +670,7 @@ while True:
         pygame.draw.rect(surface, (0,0,255,200), (i+300/1000*WIDTH, HEIGHT-vibration-water_height+15, 5/1000*WIDTH, water_height+vibration-15))
 
     
-    """
-    ls1_text = font2.render("LS1", True, (255, 255, 255))
-    screen.blit(ls1_text, (755, 65))
-    ls1_text = font2.render("LS2", True, (255, 255, 255))
-    screen.blit(ls1_text, (805, 65))
-    ls1_text = font2.render("LS3", True, (255, 255, 255))
-    screen.blit(ls1_text, (805+50, 65))
-    ls1_text = font2.render("LS4", True, (255, 255, 255))
-    screen.blit(ls1_text, (805+100, 65))
-    ls1_text = font2.render("LS5", True, (255, 255, 255))
-    screen.blit(ls1_text, (805+150, 65))
-    ls1_text = font2.render("LS6", True, (255, 255, 255))
-    screen.blit(ls1_text, (805-50, 65+50))
-    ls1_text = font2.render("LS7", True, (255, 255, 255))
-    screen.blit(ls1_text, (805, 65+50))
-    ls1_text = font2.render("LS8", True, (255, 255, 255))
-    screen.blit(ls1_text, (805+50, 65+50))
-    ls1_text = font2.render("LS9", True, (255, 255, 255))
-    screen.blit(ls1_text, (805+100, 65+50))
-    ls1_text = font2.render("LS10", True, (255, 255, 255))
-    screen.blit(ls1_text, (805+150, 65+50))
-    ls1_text = font2.render("LS11", True, (255, 255, 255))
-    screen.blit(ls1_text, (805-50, 65+100))
-    ls1_text = font2.render("LS12", True, (255, 255, 255))
-    screen.blit(ls1_text, (805, 65+100))
-    ls1_text = font2.render("LS13", True, (255, 255, 255))
-    screen.blit(ls1_text, (805+50, 65+100))
-    ls1_text = font2.render("LS14", True, (255, 255, 255))
-    screen.blit(ls1_text, (805+100, 65+100))
-    ls1_text = font2.render("LS15", True, (255, 255, 255))
-    screen.blit(ls1_text, (805+150, 65+100))
-    ls1_text = font2.render("LS16", True, (255, 255, 255))
-    screen.blit(ls1_text, (805-50, 65+150))
-    ls1_text = font2.render("LS17", True, (255, 255, 255))
-    screen.blit(ls1_text, (805, 65+150))
-    ls1_text = font2.render("LS18", True, (255, 255, 255))
-    screen.blit(ls1_text, (805+50, 65+150))
-    ls1_text = font2.render("LS19", True, (255, 255, 255))
-    screen.blit(ls1_text, (805+100, 65+150))
-    ls1_text = font2.render("LS20", True, (255, 255, 255))
-    screen.blit(ls1_text, (805+150, 65+150))
-    ls1_text = font2.render("LS21", True, (255, 255, 255))
-    screen.blit(ls1_text, (805-50, 65+200))
-    ls1_text = font2.render("LS22", True, (255, 255, 255))
-    screen.blit(ls1_text, (805, 65+200))
-    ls1_text = font2.render("LS23", True, (255, 255, 255))
-    screen.blit(ls1_text, (805+50, 65+200))
-    ls1_text = font2.render("LS24", True, (255, 255, 255))
-    screen.blit(ls1_text, (805+100, 65+200))
-    ls1_text = font2.render("LS25", True, (255, 255, 255))
-    screen.blit(ls1_text, (805+150, 65+200))
-    ls1_text = font2.render("LS26", True, (255, 255, 255))
-    screen.blit(ls1_text, (805-50, 65+250))
-    ls1_text = font2.render("LS27", True, (255, 255, 255))
-    screen.blit(ls1_text, (805, 65+250))
-    ls1_text = font2.render("LS28", True, (255, 255, 255))
-    screen.blit(ls1_text, (805+50, 65+250))
-    ls1_text = font2.render("LS29", True, (255, 255, 255))
-    screen.blit(ls1_text, (805+100, 65+250))
-    ls1_text = font2.render("LS30", True, (255, 255, 255))
-    screen.blit(ls1_text, (805+150, 65+250))
-    ls1_text = font2.render("LS31", True, (255, 255, 255))
-    screen.blit(ls1_text, (805-50, 65+300))
-    ls1_text = font2.render("LS32", True, (255, 255, 255))
-    screen.blit(ls1_text, (805, 65+300))
-    """
-
+    
     # Render and display the blue box height
     pygame.draw.rect(surface, GRAY, (5/1000*WIDTH, 5/500*HEIGHT, 280/1000*WIDTH, 60/500*HEIGHT))
     height_text = font.render(f"Water Level: {box_height:.3f} m", True, (0,0,0))
